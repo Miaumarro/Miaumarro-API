368a9b6654e2d29fa0a0d3fb21fab5448aaebbba
Kaoticz <1812311+Kaoticz@users.noreply.github.com>
2022-07-22 18:37:26 -0300

    Added unit tests

---
+++ MiauTests/MiauDatabase/InjectionTest.cs
@@ -0,0 +1,29 @@
+using MiauDatabase;
+using MiauDatabase.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MiauTests.MiauDatabase;
+
+public sealed class InjectionTest
+{
+    private readonly IServiceProvider _ioc;
+    private readonly IServiceScopeFactory _scopeFactory;
+
+    public InjectionTest()
+    {
+        _ioc = new ServiceCollection()
+            .AddMiauDb(false)
+            .BuildServiceProvider();
+
+        _scopeFactory = _ioc.GetRequiredService<IServiceScopeFactory>();
+    }
+
+    [Fact]
+    internal MiauDbContext MiauDbInjectionTest()
+    {
+        using var scope = _scopeFactory.CreateScope();
+
+        // This must not throw
+        return scope.ServiceProvider.GetRequiredService<MiauDbContext>();
+    }
+}
