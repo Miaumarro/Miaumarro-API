741fd246cd725b01a4de90f31a1dac6efa6c36af
Kaoticz <1812311+Kaoticz@users.noreply.github.com>
2022-07-22 18:26:49 -0300

    Forgot the design file

---
+++ MiauDatabase/Design/MiauDbContextFactory.cs
@@ -0,0 +1,13 @@
+using MiauDatabase.Common;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MiauDatabase.Config;
+
+/// <summary>
+/// This class is only used at design time, when EF Core is asked to perform a migration.
+/// </summary>
+internal sealed class MiauDbContextFactory : IDesignTimeDbContextFactory<MiauDbContext>
+{
+    public MiauDbContext CreateDbContext(string[] args)
+        => new(MiauDbStatics.MiauDbDefaultOptions);
+}
