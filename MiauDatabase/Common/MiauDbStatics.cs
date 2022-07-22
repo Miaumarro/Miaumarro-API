9dc95af59042ceead52a8ef8e5c6ed55056ea282
Kaoticz <1812311+Kaoticz@users.noreply.github.com>
2022-07-22 19:11:16 -0300

    Reverted access level to the default connection string

--- MiauDatabase/Common/MiauDbStatics.cs
+++ MiauDatabase/Common/MiauDbStatics.cs
@@ -12,14 +12,14 @@ internal static class MiauDbStatics
     /// SQLite database connection string.
     /// </summary>
     /// <remarks>Points to the current directory of the application. Has the format "Data Source=Miau.db"</remarks>
-    internal static readonly string MiauDbConnectionString = "Data Source=" + Path.Combine(Directory.GetParent(Assembly.GetExecutingAssembly().Location)?.FullName ?? string.Empty, "Miau.db");
+    private static readonly string _miauDbConnectionString = "Data Source=" + Path.Combine(Directory.GetParent(Assembly.GetExecutingAssembly().Location)?.FullName ?? string.Empty, "Miau.db");
 
     /// <summary>
     /// Default options for a <see cref="MiauDbContext"/>
     /// </summary>
     internal static DbContextOptions<MiauDbContext> MiauDbDefaultOptions { get; } = new DbContextOptionsBuilder<MiauDbContext>()
         .UseSnakeCaseNamingConvention()     // Set column names to snake_case format
-        .UseSqlite(MiauDbConnectionString)  // Database connection string
+        .UseSqlite(_miauDbConnectionString)  // Database connection string
         .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking) // Disable EF Core entity tracking - see https://docs.microsoft.com/en-us/ef/core/change-tracking/
         .Options;
 
@@ -31,7 +31,7 @@ internal static class MiauDbStatics
     internal static DbContextOptionsBuilder GetDefaultDbOptions(DbContextOptionsBuilder options)
     {
         return options.UseSnakeCaseNamingConvention()   // Set column names to snake_case format
-            .UseSqlite(MiauDbConnectionString)          // Database connection string
+            .UseSqlite(_miauDbConnectionString)          // Database connection string
             .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);    // Disable EF Core entity tracking - see https://docs.microsoft.com/en-us/ef/core/change-tracking/
     }
 }
