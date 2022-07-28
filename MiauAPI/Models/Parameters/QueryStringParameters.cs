a6aeb08ff1884569b170d8a1ecab042d398cdfd3
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-07-27 22:02:52 -0400

    Implemented QueryStringParameters class for abstraction behavior

---
+++ MiauAPI/Models/Parameters/QueryStringParameters.cs
@@ -0,0 +1,19 @@
+namespace MiauAPI.Models.Parameters;
+
+public abstract class QueryStringParameters
+{
+    private const int _maxPageSize = 50;
+    public int PageNumber { get; set; } = 1;
+    private int _pageSize = 20;
+    public int PageSize
+    {
+        get
+        {
+            return _pageSize;
+        }
+        set
+        {
+            _pageSize = (value > _maxPageSize) ? _maxPageSize : value;
+        }
+    }
+}
