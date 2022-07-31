2be22f68a579e99e639fa11f1b5cc7c0b0be201f
Kaoticz <1812311+Kaoticz@users.noreply.github.com>
2022-07-31 12:28:01 -0300

    Added column Name to Products table

---
+++ MiauDatabase/Migrations/20220731152107_ProductName.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiauDatabase.Migrations;
+
+public partial class ProductName : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>(
+            name: "name",
+            table: "products",
+            type: "TEXT",
+            maxLength: 50,
+            nullable: false,
+            defaultValue: "");
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "name",
+            table: "products");
+    }
+}
