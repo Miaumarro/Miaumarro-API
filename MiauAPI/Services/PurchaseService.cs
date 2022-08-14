2bc2ba6e182abd6fad645fdfbb5609cd2be054fe
Kaoticz <1812311+Kaoticz@users.noreply.github.com>
2022-08-14 18:30:57 -0300

    Possible improvement of query for purchased products

--- MiauAPI/Services/PurchaseService.cs
+++ MiauAPI/Services/PurchaseService.cs
@@ -30,7 +30,7 @@ public sealed class PurchaseService
     /// <param name="location">The URL of the new resource or the content of the Location header.</param>
     /// <returns>The result of the operation.</returns>
     /// <exception cref="ArgumentException">Occurs when <paramref name="location"/> is <see langword="null"/> or empty.</exception>
-    public async Task<ActionResult<OneOf<CreatedPurchaseResponse, ErrorResponse>>> CreatePetAsync(CreatedPurchaseRequest request, string location)
+    public async Task<ActionResult<OneOf<CreatedPurchaseResponse, ErrorResponse>>> CreatePurchaseAsync(CreatedPurchaseRequest request, string location)
     {
         if (string.IsNullOrWhiteSpace(location))
             throw new ArgumentException("Location cannot be null or empty.", nameof(location));
@@ -42,50 +42,38 @@ public sealed class PurchaseService
         // Create the database purchase
         var dbPurchaseTemp = new PurchaseEntity()
         {
-            User = _db.Users.First(x => x.Id == request.UserId),
-            Coupon = _db.Coupons.First(x => x.Id == request.CouponId),
+            User = await _db.Users.FirstAsync(x => x.Id == request.UserId),
+            Coupon = await _db.Coupons.FirstOrDefaultAsync(x => x.Id == request.CouponId),
             Status = request.Status
         };
-        var purchasedProducts = CreatePurchasedProductsList(request.ProductsId, dbPurchaseTemp.Id);
+        var purchasedProducts = await CreatePurchasedProductsListAsync(request.ProductsId, dbPurchaseTemp.Id);
         var dbPurchase = new PurchaseEntity()
         {
             Id = dbPurchaseTemp.Id,
             PurchasedProduct = purchasedProducts
         };
+
         _db.Purchases.Add(dbPurchase);
         await _db.SaveChangesAsync();
 
-
-        // TODO: handle authentication properly
         return new CreatedResult(location, new CreatedPurchaseResponse(dbPurchase.Id));
     }
-    private sealed record PurchasedProductRequest(int Id, decimal SalePrice);
-    private List<PurchasedProductEntity> CreatePurchasedProductsList(List<int> productsId, int purchaseId)
-    {
-        decimal salePrice = 0;
-        ProductEntity purchased;
-        PurchasedProductEntity dbPurchasedProduct;
-        var purchasedProducts = new List<PurchasedProductRequest>();
-        foreach (var product in productsId)
-        {
-            purchased = _db.Products.First(x => x.Id == product);
-            salePrice = purchased.Price - (purchased.Price*purchased.Discount);
-            purchasedProducts.Add(new PurchasedProductRequest(product, salePrice));
-        }
 
-        var purchasedProductsList = new List<PurchasedProductEntity>();
-        foreach(var purchase in purchasedProducts)
-        {
-            dbPurchasedProduct = new PurchasedProductEntity()
+    private async Task<List<PurchasedProductEntity>> CreatePurchasedProductsListAsync(List<int> productsId, int purchaseId)
+    {
+        var purchasedProducts = await _db.Products
+            .Where(x => productsId.Contains(x.Id))
+            .Select(x => new PurchasedProductEntity()
             {
-                Product = _db.Products.First(x => x.Id == purchase.Id),
+                Product = x,
                 Purchase = _db.Purchases.First(x => x.Id == purchaseId),
-                SalePrice = purchase.SalePrice
-            };
-            purchasedProductsList.Add(dbPurchasedProduct);
-            _db.PurchasedProducts.Add(dbPurchasedProduct);
-        }
-        _db.SaveChanges();
-        return purchasedProductsList;
+                SalePrice = x.Price - (x.Price * x.Discount)
+            })
+            .ToListAsync();
+
+        _db.PurchasedProducts.AddRange(purchasedProducts);
+        await _db.SaveChangesAsync();
+
+        return purchasedProducts;
     }
 }
